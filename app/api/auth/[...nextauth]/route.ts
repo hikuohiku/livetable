562f82b93195136ab4672b308f9774150ee150a2
Hiroki Ando <hikuohiku@gmail.com>
2024-02-18 19:32:58 +0900

    add: nextauth callbacks

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth/next";
-import { getServerSession } from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaClient } from "@prisma/client";
+import { v4 as uuidv4 } from "uuid";
 
 const clientId = process.env.GOOGLE_CLIENT_ID;
 const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
@@ -12,6 +13,8 @@ if (!clientSecret) {
   throw new Error("GOOGLE_CLIENT_SECRET is not set");
 }
 
+const prisma = new PrismaClient();
+
 const handler = NextAuth({
   providers: [
     GoogleProvider({
@@ -19,6 +22,40 @@ const handler = NextAuth({
       clientSecret: clientSecret,
     }),
   ],
+  callbacks: {
+    async jwt({ token, account, profile }) {
+      // サインインのときだけ発火
+      if (account && account.provider === "google" && profile) {
+        const uuid = token.uuid ? token.uuid as string : uuidv4();
+        const email = profile.email as string;
+        const name = profile.name as string;
+
+        const user = await prisma.user.upsert({
+          where: { email: email },
+          update: { name: name },
+          create: { uuid: uuid, email: email, name: name },
+        })
+
+        const userId = user.uuid;
+        const refreshToken = account.refresh_token as string;
+        const accessToken = account.access_token as string;
+
+        await prisma.googleUser.upsert({
+          where: { userId: userId },
+          update: { refreshToken: refreshToken, accessToken: accessToken },
+          create: { userId: userId, refreshToken: refreshToken, accessToken: accessToken },
+        })
+
+        token = {
+          ...token,
+          uuid: user.uuid,
+        }
+        return token;
+      }
+
+      return token;
+    }
+  }
 });
 
 export { handler as GET, handler as POST };
