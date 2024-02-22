ed9218549ff9e855733a2b5c363e43e67993adbb
Hiroki <hikuohiku@gmail.com>
2024-02-22 19:16:57 +0900

    fix: find()でidが一致するものを探すように

--- services/youtubeApiService.ts
+++ services/youtubeApiService.ts
@@ -4,7 +4,6 @@ import { google } from "googleapis";
 import { youtube } from "googleapis/build/src/apis/youtube";
 
 const apiKey = process.env.YOUTUBE_API_KEY;
-
 if (!apiKey) {
   throw new Error("YOUTUBE_API_KEY is not defined");
 }
@@ -12,44 +11,39 @@ if (!apiKey) {
 const youtubeApiService = google.youtube('v3');
 
 export async function getStartAtTime(streams: Stream[]): Promise<Stream[]> {
-  const responseData = await youtubeApiService.videos.list({
-    key: apiKey,
-    part: ['liveStreamingDetails'],
-    id: streams.map(s => s.videoId),
-  }).then(response => response.data);
+  try {
+    const responseData = await youtubeApiService.videos.list({
+      key: apiKey,
+      part: ['liveStreamingDetails'],
+      id: streams.map(s => s.videoId),
+    }).then(response => response.data);
 
-  // レスポンスのバリデーション
-  if (!responseData.items) {
-    throw new Error("Invalid response data from YouTube API");
-  }
-  const responseItems = responseData.items
-  responseItems.forEach((item, i) => {
-    if (!(item.id == streams[i].videoId)) {
+    // レスポンスのバリデーション
+    if (!responseData.items) {
       throw new Error("Invalid response data from YouTube API");
     }
-  });
+    const responseItems = responseData.items
 
-  // startAtをstreamsに追加
-  const streamWithStartAtTime = streams.map((s, i) => {
-    const item = responseItems[i];
-    if (!item) {
-      return s;
-    }
+    // startAtをstreamsに追加
+    const streamWithStartAtTime = streams.map((s) => {
+      const item = responseItems.find(item => item.id === s.videoId);
 
-    const streamingDetails = item.liveStreamingDetails;
-    if (!streamingDetails) {
-      return s;
-    }
-    const startAt = streamingDetails.scheduledStartTime ? streamingDetails.scheduledStartTime : streamingDetails.actualStartTime;
-    if (!startAt) {
-      return s;
-    }
+      if (!item || !item.liveStreamingDetails) {
+        return s;
+      }
+      const streamingDetails = item.liveStreamingDetails;
 
-    return {
-      ...s,
-      startAt,
-    };
-  });
+      const { actualStartTime, scheduledStartTime } = streamingDetails;
+      const startAt = actualStartTime || scheduledStartTime;
 
-  return streamWithStartAtTime;
+      return startAt ? { ...s, startAt } : s;
+    });
+
+    return streamWithStartAtTime;
+
+  } catch (e) {
+    // TODO: エラー処理
+    console.error(e);
+    throw new Error("Failed to fetch data from YouTube API");
+  }
 }
