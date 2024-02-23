797e3c2333ce317d36f2337c853700081ec38812
Hiroki <hikuohiku@gmail.com>
2024-02-23 16:03:07 +0900

    up: ChannelInfoServiceインターフェースを追加，実装

--- services/youtubeService.ts
+++ services/youtubeService.ts
@@ -1,16 +1,21 @@
 import StreamInfoService from "@/types/services/streamInfoService";
+import ChannelInfoService from "@/types/services/channelInfoService";
 import youtubeRssService from "@/services/youtubeRssService";
 import youtubeApiService from "@/services/youtubeApiService";
 import Channel from "@/types/entities/channel";
 import Stream from "@/types/entities/stream";
 
-export class YoutubeService implements StreamInfoService {
+export class YoutubeService implements StreamInfoService, ChannelInfoService {
   async getStreams(channel: Channel): Promise<Stream[]> {
     return await youtubeRssService.getStreams(channel);
   }
 
   async getStartAtTime(streams: Stream[]): Promise<Stream[]> {
     return await youtubeApiService.getStartAtTime(streams);
+  }
+
+  async getChannelsInfo(channel: Channel[]): Promise<Channel[]> {
+    return await youtubeApiService.getChannel(channel);
   }
 }
 
