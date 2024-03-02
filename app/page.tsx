d8cad989b3e4e5002e41934b7cbcd17429f79cf6
Hiroki Ando <hikuohiku@gmail.com>
2024-03-02 15:28:14 +0900

    up: デバッグ用コードを削除

--- app/page.tsx
+++ app/page.tsx
@@ -6,63 +6,6 @@ import Typography from '@mui/material/Typography';
 import MenuIcon from '@mui/icons-material/Menu';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 
-/* 確認用
-import youtubeRssService from '@/services/youtubeRssService';
-
-youtubeRssService.getStreams({ channelId: 'UCt30jJgChL8qeT9VPadidSw' }).then((streams) => {
-  console.log(streams);
-}
-);
-*/
-
-/* 確認用
-import youtubeApiService from '@/services/youtubeApiService';
-import Stream from '@/types/entities/stream';
-
-const streams: Stream[] = [
-  {
-    videoId: 'zoHoVtb-yww',
-    channelId: 'UCvaTdHTWBGv3MKj3KVqJVCw',
-  },
-  {
-    videoId: 'T0-I9G9w7hA',
-    channelId: 'UCvaTdHTWBGv3MKj3KVqJVCw',
-  },
-];
-
-youtubeApiService.getStartAtTime(streams).then((streams) => {
-  console.log(streams);
-});
-*/
-
-/* 確認用
-import youtubeApiService from '@/services/youtubeApiService';
-import { GoogleUser } from '@/types/entities/user';
-
-const user: GoogleUser = {
-  uuid: '1',
-  email: '1',
-  token: 'ya29.a0AfB_byDXvXIQT2VdyMYtBd5Luf9n0sGaQTEfljNu2GO5bYk0icC1A1djxUvd5kIhav5flnlOU0MJHEo13oNxpuYdAbB_F9bp7Hfc-BBGP-dMpJRm1N_kmaMJQEmQyHzmJ4drSbyHWlxr5Lm-N3TLf-IoGMcDJmROzVpnaCgYKAcMSARMSFQHGX2MiJwtAblXIf7f5574vT1pScQ0171',
-};
-
-youtubeApiService.getSubscription(user).then((subscriptions) => {
-  console.log(subscriptions);
-});
-*/
-
-/* 確認用
-import youtubeApiService from '@/services/youtubeApiService';
-import Channel from '@/types/entities/channel';
-
-const channel: Channel = {
-  channelId: 'UCIBY1ollUsauvVi4hW4cumw',
-};
-
-youtubeApiService.getChannel([channel]).then((channel) => {
-  console.log(channel);
-});
-*/
-
 export default function Page() {
   return (
     <AppBar>
