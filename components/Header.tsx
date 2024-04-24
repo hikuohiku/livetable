12fd5ae720c9c14992ea662e031429d0ba1556c1
Hiroki Ando <hikuohiku@gmail.com>
2024-04-24 15:34:55 +0900

    add: Header.tsx ヘッダーコンポーネントを追加

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+import { varelaRound } from '../utils/font';
+import UserIcon from './UserIcon';
+
+function Header() {
+  return (
+    <header className='z-[10] fixed flex justify-between items-center inset-0 h-16'>
+      <h1 className={`${varelaRound.className} block text-2xl text-logo mx-6`}>livetable</h1>
+      <UserIcon />
+    </header>
+  );
+}
+
+export default Header;
