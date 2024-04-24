1155f668b3ed8c2ebc8c6c921cf43527012c88ad
Hiroki Ando <hikuohiku@gmail.com>
2024-04-24 15:53:45 +0900

    add: Splitter.tsx Splitterコンポーネントを追加

---
+++ components/Splitter.tsx
@@ -0,0 +1,18 @@
+import TimeTip from './TimeTip';
+
+interface SplitterProps {
+  time: Date;
+}
+
+function Splitter({ time }: SplitterProps) {
+  return (
+    <div className='relative flex items-center w-full'>
+      <div className='absolute left-0 -translate-x-full mt-[3px]'>
+        <TimeTip time={time} />
+      </div>
+      <div className='splitter flex-1 mx-2 my-2' />
+    </div>
+  );
+}
+
+export default Splitter;
