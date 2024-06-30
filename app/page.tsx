35851a9a7c03eb7d163b427018ce3fee63db3b96
Hiroki Ando <hikuohiku@gmail.com>
2024-07-01 00:23:18 +0900

    up: `app/page.tsx` とりあえずfetchLivesの原型

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,30 @@ import CardContainer from '@/components/CardContainer';
 import Header from '@/components/Header';
 import LiveCard from '@/components/LiveCard';
 import Splitter from '@/components/Splitter';
+import userRepository, { subscriptionRepository } from '@/services/repositories/userRepository';
+import videoRepository from '@/services/repositories/videoRepository';
+import { getServerSession } from 'next-auth';
+import { authOptions } from './api/auth/[...nextauth]/route';
+
+async function fetchLives() {
+  const session = await getServerSession(authOptions);
+  if (!session) return;
+  const email = session.user?.email;
+  if (!email) return;
+  const user = await userRepository.findByEmail(email);
+  if (!user) return;
+  const subscriptions = await subscriptionRepository.findByUser(user);
+  if (!subscriptions) return;
+  const lives = await Promise.all(
+    subscriptions.map(async (subscription) => {
+      return await videoRepository.findByChannelId(subscription.channelId);
+    }),
+  );
+  return lives;
+}
 
 export default function Page() {
+  const lives = fetchLives();
   return (
     <>
       <Header />
