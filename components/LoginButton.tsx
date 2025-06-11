ea65f800772f49b8fe9cedcb86459f8cfb8ebb20
hikuohiku <hikuohiku@gmail.com>
2025-06-11 20:35:18 +0900

    ログインボタンをいい感じに

--- components/LoginButton.tsx
+++ components/LoginButton.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { signIn } from 'next-auth/react';
+import Google from '@mui/icons-material/Google';
 
 interface LoginButtonProps {
   onClick: React.MouseEventHandler<HTMLButtonElement>;
@@ -7,9 +8,15 @@ interface LoginButtonProps {
 
 const LoginButton = ({ onClick }: LoginButtonProps) => {
   return (
-    <button onClick={onClick} className='glass flex justify-center items-center gap-2 rounded-lg m-2 p-1 w-72'>
-      Google でログイン
-    </button>
+    <div className="flex justify-center items-center w-full py-8">
+      <button 
+        onClick={onClick} 
+        className='glass flex justify-center items-center gap-3 rounded-lg px-6 py-3 text-lg font-medium shadow-lg hover:shadow-xl transition-all duration-300'
+      >
+        <Google fontSize="medium" />
+        Google でログイン
+      </button>
+    </div>
   );
 };
 
